a6417a1a8be4dbcf87dbe56903732aaae1a2b5c3
ParveenMalethia <prince.malethia.36@gmail.com>
2021-07-05 22:18:21 +0530

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,65 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-app-bar
+      app
+      color="deep-purple darken-1"
+      dark
+    >
+      <div class="d-flex align-center">
+        <h2>Esportify</h2>
+      </div>
+
+      <v-spacer></v-spacer>
+    </v-app-bar>
+
+    <v-main >
+      <v-container class="pa-4 text-center">
+        <h2>Your Playlist</h2>
+        <v-btn
+      class="ma-2"
+      :loading="loading"
+      :disabled="loading"
+      color="secondary"
+      @click="LoadSongs"
+    >
+      Refresh Playlist
+    </v-btn>
+      </v-container>
+      <div v-for="item in items" :key="item.id">
+        <Song :song="item"/>
+      </div>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Song from './components/Song';
+import axios from 'axios';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  components: {
+    Song,
+  },
+
+  data: () => ({
+    items:[],
+    loading:false,
+  }),
+  mounted () {
+    this.LoadSongs()
+    },
+  methods:{
+    LoadSongs(){
+      this.loading = true
+      axios
+      .get('http://localhost:8000/')
+        .then(response => {
+          this.items = response.data
+          this.loading = false
+    })
+    }
+  }
+};
+</script>
---
+++ src/components/Song.vue
@@ -0,0 +1,163 @@
+<template>
+  <v-container class="pa-4 text-center">
+    <v-row class="fill-height" align="center" justify="center">
+      <template>
+        <v-col cols="12" md="4">
+          <v-hover v-slot="{ hover }">
+            <v-card
+              :elevation="hover ? 12 : 2"
+              class="on-hover"
+              color="green lighten-2"
+            >
+              <v-card-title class="text-h6">
+                <v-row class="fill-height flex-column" justify="space-between">
+                  <p class="mt-4 ml-4 subheading text-center">
+                    {{ song.name }} <v-icon v-if="playing" class="ml-2">mdi-music</v-icon>
+                  </p>
+                  <v-slider
+                    v-model="volume"
+                    :prepend-icon="muted ? 'mdi-volume-off' : 'mdi-volume-high'"
+                    @click:prepend="mute(song.id)"
+                    color="green darken-4"
+                    @change="setVolume(song.id, volume)"
+                    track-color="white"
+                    class="ml-4 mr-4"
+                    thumb-color="green darken-4"
+                    min="0"
+                    max="9"
+                  ></v-slider>
+                  <div class="align-self-center">
+                    <v-btn
+                      @click="previous(song.id)"
+                      :key="1"
+                      class="show-btns"
+                      :color="transparent"
+                      icon
+                    >
+                      <v-icon
+                        :class="{ 'show-btns': hover }"
+                        :color="transparent"
+                      >
+                        mdi-rewind
+                      </v-icon>
+                    </v-btn>
+                    <v-btn
+                      @click="togglePlay(song.id)"
+                      :key="2"
+                      :class="{ 'show-btns': hover }"
+                      :color="transparent"
+                      icon
+                    >
+                      <v-icon
+                        :class="{ 'show-btns': hover }"
+                        :color="transparent"
+                      >
+                        {{ playing ? "mdi-pause" : "mdi-play" }}
+                      </v-icon>
+                    </v-btn>
+                    <v-btn
+                      @click="next(song.id)"
+                      :key="3"
+                      :class="{ 'show-btns': hover }"
+                      :color="transparent"
+                      icon
+                    >
+                      <v-icon
+                        :class="{ 'show-btns': hover }"
+                        :color="transparent"
+                      >
+                        mdi-fast-forward
+                      </v-icon>
+                    </v-btn>
+                  </div>
+                </v-row>
+              </v-card-title>
+            </v-card>
+          </v-hover>
+        </v-col>
+        <audio :id="'music' + song.id">
+          <source :src="`http://localhost:8000${song.song}`" type="audio/ogg" />
+        </audio>
+      </template>
+    </v-row>
+  </v-container>
+</template>
+<script>
+
+export default {
+  props: {
+    song: {
+      type: Object,
+      required: true,
+    },
+  },
+  data: () => ({
+    playing: false,
+    transparent: "black",
+    otherPlaying: false,
+    volume: 9,
+    muted: false,
+  }),
+  methods: {
+    previous(id) {
+      let music = document.getElementById("music" + id);
+      music.currentTime = music.currentTime - 10;
+    },
+
+    togglePlay(id) {
+      let music = document.getElementById("music" + id);
+      music.play();
+      this.otherPlaying = true;
+      if (this.playing) {
+        music.pause();
+        this.playing = false;
+      } else {
+        music.play();
+        this.playing = true;
+      }
+    },
+
+    next(id) {
+      let music = document.getElementById("music" + id);
+      console.log(music.currentTime);
+      music.currentTime = music.currentTime + Number(10);
+    },
+    mute(id) {
+      let music = document.getElementById("music" + id);
+      if (this.muted) {
+        music.muted = false;
+        this.muted = false;
+      } else {
+        music.muted = true;
+        this.muted = true;
+      }
+    },
+    intToFloat(num, decPlaces) {
+      return 0 + "." + String(num)[0] + Array(decPlaces + 2).join("0");
+    },
+    setVolume(id, volume) {
+      let vol = this.intToFloat(volume, 1);
+      if (Number(vol) == 0) {
+        this.muted = true;
+      } else {
+        this.muted = false;
+      }
+      var music = document.getElementById("music" + id);
+      music.volume = Number(vol);
+    },
+  },
+};
+</script>
+<style scoped>
+.v-card {
+  transition: opacity 0.4s ease-in-out;
+}
+
+.v-card:not(.on-hover) {
+  opacity: 0.6;
+}
+
+.show-btns {
+  color: rgba(255, 255, 255, 1) !important;
+}
+</style>
